24636a4127c6bc995f172b13e2984bbc40aa5448
Liseth Cardozo Sejas <cardozoliseth@gmail.com>
2018-03-07 00:18:31 -0800

    create a new class and a method

---
+++ ReverseWords/ReverseWords/Subtraction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseWords
+{
+    class Subtraction
+    {
+
+        public int SubtractionOfTwoNumbers(int num1, int num2)
+        {
+            return (num1 - num2);
+        }
+    }
+    
+}
